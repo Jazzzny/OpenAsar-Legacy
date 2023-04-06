b68b3a089b0e30d23cce972d5cd6c7d2133b8d7b
Jazzzny <jazzzny225@gmail.com>
2023-04-06 19:16:18 -0400

    test3

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -3,17 +3,15 @@ const { readFileSync } = require('fs');
 const { join } = require('path');
 
 if (!String.prototype.replaceAll) {
-	String.prototype.replaceAll = function(str, newStr){
+  String.prototype.replaceAll = function (str, newStr) {
+    // If a regex pattern
+    if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
+      return this.replace(str, newStr);
+    }
 
-		// If a regex pattern
-		if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
-			return this.replace(str, newStr);
-		}
-
-		// If a string
-		return this.replace(new RegExp(str, 'g'), newStr);
-
-	};
+    // If a string
+    return this.replace(new RegExp(str, 'g'), newStr);
+  };
 }
 
 if (!settings.get('enableHardwareAcceleration', true)) app.disableHardwareAcceleration();
