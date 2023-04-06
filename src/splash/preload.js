4f92bb263c74cbf0aac2ffe7401aad9e89eda769
Jazzzny <jazzzny225@gmail.com>
2023-04-06 18:23:45 -0400

    Try to fix

--- src/splash/preload.js
+++ src/splash/preload.js
@@ -2,7 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 
 contextBridge.exposeInMainWorld('Splash', {
-  onState: callback => ipcRenderer.on('state', (_, state) => callback(state)),
-  quit: () => ipcRenderer.send('sq'),
-  skip: () => ipcRenderer.send('ss')
+  onState: callback => ipcRenderer.on('DISCORD_SPLASH_UPDATE_STATE', (_, state) => callback(state)),
+  quit: () => ipcRenderer.send('DISCORD_SPLASH_SCREEN_QUIT'),
+  skip: () => ipcRenderer.send('DISCORD_SPLASH_SCREEN_READY')
 });
+
