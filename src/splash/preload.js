efa5ed8eb7059bf9f4277ad84bb20c41c5d8072e
Jazzzny <jazzzny225@gmail.com>
2023-04-06 18:48:41 -0400

    Revert "Try to fix"
    This reverts commit 4f92bb263c74cbf0aac2ffe7401aad9e89eda769.

--- src/splash/preload.js
+++ src/splash/preload.js
@@ -2,8 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 
 contextBridge.exposeInMainWorld('Splash', {
-  onState: callback => ipcRenderer.on('DISCORD_SPLASH_UPDATE_STATE', (_, state) => callback(state)),
-  quit: () => ipcRenderer.send('DISCORD_SPLASH_SCREEN_QUIT'),
-  skip: () => ipcRenderer.send('DISCORD_SPLASH_SCREEN_READY')
+  onState: callback => ipcRenderer.on('state', (_, state) => callback(state)),
+  quit: () => ipcRenderer.send('sq'),
+  skip: () => ipcRenderer.send('ss')
 });
-
